1566d07e4a76459546714ff520f323b628552ff4
Philipp <philippaaron@posteo.de>
2021-12-04 01:20:38 +0100

    NOT WORKING add HLS support for iPad/iPhone/iPod video.play() is generally blocked. Must be an onclick event. the if statement does not work either. somehow flv.play() is executed.

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
-if (flvjs.isSupported()) {
-    var videoElement = document.getElementById('videoElement');
-    var flvPlayer = flvjs.createPlayer({
-        type: 'flv',
-        url: 'https://stream.moafunk.de/live/stream-io.flv'
-    });
-    flvPlayer.attachMediaElement(videoElement);
-    flvPlayer.load();
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+if (true) {
+    var video = document.getElementById('videoElement');
+    var videoSrc = 'https://stream.moafunk.de/live/stream-io/index.m3u8';
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+    }
 
     function play() {
         let btn = document.getElementById('btn-play');
         let playing = btn.className.includes("btn-pause");
         if (playing) {
-            flvPlayer.pause();
+            video.pause();
             btn.className = "btn";
         } else {
-            flvPlayer.play();
+            video.play();
             btn.className = "btn btn-pause";
         }
     }
+} else {
+    if (flvjs.isSupported()) {
+        var videoElement = document.getElementById('videoElement');
+        var flvPlayer = flvjs.createPlayer({
+                                           type: 'flv',
+                                           url: 'https://stream.moafunk.de/live/stream-io.flv'
+                                           });
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.load();
+
+        function play() {
+            let btn = document.getElementById('btn-play');
+            let playing = btn.className.includes("btn-pause");
+            if (playing) {
+                flvPlayer.pause();
+                btn.className = "btn";
+            } else {
+                flvPlayer.play();
+                btn.className = "btn btn-pause";
+            }
+        }
+    }
+
 }
