bbd2cd76652d7224e545cc6ccb4dd1534a3074f4
mpanne <merlin.pannewitz@bosch.io>
2022-01-11 09:57:02 +0100

    Fix condition for iOS; cleanup JS.

--- main.js
+++ main.js
@@ -1,45 +1,31 @@
-var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if (true) {
-    var video = document.getElementById('videoElement');
-    var videoSrc = 'https://stream.moafunk.de/live/stream-io/index.m3u8';
+let isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+let video = document.getElementById('videoElement');
+if (isIOS) {
+    let videoSrc = 'https://stream.moafunk.de/live/stream-io/index.m3u8';
     if (Hls.isSupported()) {
-        var hls = new Hls();
+        let hls = new Hls();
         hls.loadSource(videoSrc);
         hls.attachMedia(video);
     }
-
-    function play() {
-        let btn = document.getElementById('btn-play');
-        let playing = btn.className.includes("btn-pause");
-        if (playing) {
-            video.pause();
-            btn.className = "btn";
-        } else {
-            video.play();
-            btn.className = "btn btn-pause";
-        }
-    }
 } else {
     if (flvjs.isSupported()) {
-        var videoElement = document.getElementById('videoElement');
-        var flvPlayer = flvjs.createPlayer({
-                                           type: 'flv',
-                                           url: 'https://stream.moafunk.de/live/stream-io.flv'
-                                           });
+        let flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            url: 'https://stream.moafunk.de/live/stream-io.flv'
+        });
         flvPlayer.attachMediaElement(videoElement);
         flvPlayer.load();
-
-        function play() {
-            let btn = document.getElementById('btn-play');
-            let playing = btn.className.includes("btn-pause");
-            if (playing) {
-                flvPlayer.pause();
-                btn.className = "btn";
-            } else {
-                flvPlayer.play();
-                btn.className = "btn btn-pause";
-            }
-        }
     }
+}
 
+function play() {
+    let btn = document.getElementById('btn-play');
+    let playing = btn.className.includes("btn-pause");
+    if (playing) {
+        video.pause();
+        btn.className = "btn";
+    } else {
+        video.play();
+        btn.className = "btn btn-pause";
+    }
 }
