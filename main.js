a37da546834331b12f5a728f51de900d6c7c8a44
Philipp <philippaaron@posteo.de>
2022-11-29 20:22:45 +0100

    add parsing manifest

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ if (flvjs.isSupported()) {
     let hls = new Hls();
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        video.play();
+      });
 }
 
 
