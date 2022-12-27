5b69878b06cbb1493370ddc09fc658ed6bb7bb6e
Philipp <philippaaron@posteo.de>
2022-12-27 16:23:57 +0100

    solution with one button

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-let video = document.getElementById('videoElement');
 
-if (flvjs.isSupported()) {
+let video;
+let btn;
+let playing;
+
+
+if (/iPhone|iPod|iPad/.test(navigator.platform)){
+//if (true){
+    console.log('is iOS');
+    video = document.getElementById('player');
+}else if(flvjs.isSupported()){
+    console.log('flvjs is supported, this is not iOS')
+    video = document.getElementById('videoElement');
+    console.log(video);
+
     let flvPlayer = flvjs.createPlayer({
         type: 'flv',
         url: 'https://stream.moafunk.de/live/stream-io.flv'
     });
     flvPlayer.attachMediaElement(videoElement);
     flvPlayer.load();
+}else{
+    console.log(avigator.platform + ' not supported as platform for streaming!')
 }
 
-
 function play() {
-    let btn = document.getElementById('btn-play');
-    let playing = btn.className.includes("btn-pause");
+    btn = document.getElementById('btn-play');
+    playing = btn.className.includes("btn-pause");
     if (playing) {
         video.pause();
         btn.className = "btn";
